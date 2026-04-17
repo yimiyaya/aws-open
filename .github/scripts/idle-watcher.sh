873dfbcfba227875975a8e5d68ea0b6aad8fd918
foyou <35125624+lemisky@users.noreply.github.com>
2026-04-17 17:51:54 +0800

    -

--- .github/scripts/idle-watcher.sh
+++ .github/scripts/idle-watcher.sh
@@ -9,6 +9,7 @@ sleep $CHECK_INTERVAL
 while sleep $CHECK_INTERVAL; do
     if pgrep -x login > /dev/null \
     || pgrep -x mosh-server > /dev/null \
+    || pgrep -f "^sshd:.*@" > /dev/null \
     || pgrep -fx tmux > /dev/null; then
         IDLE_TIME=0
     else
